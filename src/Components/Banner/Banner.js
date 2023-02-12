3aacc16f5b44bee745d48e7354b36f00a1ad1eb6
Debashis Roy <debashisroy9191@gmail.com>
2023-02-13 01:07:13 +0600

    banner done

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import ''
+import './Banner.css';
+import bike from '../../images/header-bike.png';
+
 const Banner = () => {
     return (
-        <div>
-            
+      <div id="banner">
+        <div id='banner-txt'>
+          <p>
+            Computer-controlled digital transistorized with electronic advance
+          </p>
+          <h1>Honda CBR 300R</h1>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+            </p>
+            <button>Purchase</button>
         </div>
+        <div id='banner-img'>
+            <img src={bike} alt=''></img>
+        </div>
+      </div>
     );
 };
 
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar/Navbar';
 import './Main.css'
+import Banner from './Banner/Banner';
 const Main = () => {
     return (
         <div>
             <div id='navbar'>
                 <Navbar></Navbar>
+                <Banner></Banner>
             </div>
         </div>
     );
