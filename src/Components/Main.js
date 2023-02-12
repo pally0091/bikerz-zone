116874606f2759a236785855e722e491b78a931d
Debashis Roy <goodquote39@hotmail.com>
2023-02-13 01:37:57 +0600

    modifying

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar/Navbar';
 import './Main.css'
 import Banner from './Banner/Banner';
 import FeaturedBike from './FeaturedBike/FeaturedBike';
+import NewsFeed from './NewsFeed/NewsFeed';
 const Main = () => {
     return (
         <div>
@@ -11,6 +12,7 @@ const Main = () => {
                 <Banner></Banner>
             </div>
             <FeaturedBike></FeaturedBike>
+            <NewsFeed></NewsFeed>
         </div>
     );
 };
