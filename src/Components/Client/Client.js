1dd3d9355ec28fa157a8213d69e6d699c8ebe567
Debashis Roy <goodquote39@hotmail.com>
2023-02-15 00:02:29 +0600

    client review done

--- src/Components/Client/Client.js
+++ src/Components/Client/Client.js
@@ -1,10 +1,58 @@
 import React from 'react';
+import './Client.css';
+import c1 from '../../images/user-1.png';
+import c2 from '../../images/user-2.png';
+import c3 from '../../images/user-3.png';
+
 
 const Client = () => {
     return (
-        <div>
-            
+      <div>
+        <h1>
+          Happy <span>Clients says</span>
+        </h1>
+        <div id="client-container">
+          <card>
+            <div className="client-img">
+              <img
+                src={c1}
+                alt=""
+              ></img>
+            </div>
+            <p>
+              Slate helps you see how many more days you need to work to reach
+              your financial goal for the month and year
+            </p>
+            <p className="clients-name">Regina Miles</p>
+          </card>
+          <card>
+            <div className="client-img">
+              <img
+                src={c2}
+                alt=""
+              ></img>
+            </div>
+            <p>
+              Slate helps you see how many more days you need to work to reach
+              your financial goal for the month and year
+            </p>
+            <p className="clients-name">Cristine</p>
+          </card>
+          <card>
+            <div className="client-img">
+              <img
+                src={c3}
+                alt=""
+              ></img>
+            </div>
+            <p>
+              Slate helps you see how many more days you need to work to reach
+              your financial goal for the month and year
+            </p>
+            <p className="clients-name">Mila Jordan</p>
+          </card>
         </div>
+      </div>
     );
 };
 
