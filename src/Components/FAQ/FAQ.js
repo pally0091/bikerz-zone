e28ca1b4acffc6bd063e9168be7b7af2426e0361
Debashis Roy <goodquote39@hotmail.com>
2023-02-16 01:47:27 +0600

    faq structure ready

--- src/Components/FAQ/FAQ.js
+++ src/Components/FAQ/FAQ.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './FAQ.css';
+import faq from '../../images/faq.png';
+import Accordian from './Accordian';
 
 const FAQ = () => {
     return (
-        <div>
-            
+        <div id='faq'>
+            <h1>Frequently Asked <span>Questions</span></h1>
+            <div id='faq-container'>
+                <div id='faq-img'>
+                    <img src={faq} alt=''></img>
+                </div>
+                <div id='accor'>
+                    <Accordian></Accordian>
+                </div>
+
+            </div>
         </div>
     );
 };
